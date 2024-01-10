e2e1fd334623605233f8955c84f723e99ad4cad2
Eros Savogin <eros.sav@gmail.com>
2024-01-10 23:19:56 +0100

    Initial commit

---
+++ src/main/java/savogineros/EserciziBackEndu2w1d2/EserciziBackEndU2W1D2Application.java
@@ -0,0 +1,13 @@
+package savogineros.EserciziBackEndu2w1d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziBackEndU2W1D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserciziBackEndU2W1D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/savogineros/EserciziBackEndu2w1d2/EserciziBackEndU2W1D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package savogineros.EserciziBackEndu2w1d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EserciziBackEndU2W1D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
